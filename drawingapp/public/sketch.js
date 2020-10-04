a0008c67ffc5a9c68d9c782eed7de22c411c169b
Renan Santana <renan_ota@hotmail.com>
2020-10-04 16:16:18 -0300

    Commit inicial

---
+++ drawingapp/public/sketch.js
@@ -0,0 +1,67 @@
+let socket
+let color = '#FFF'
+let strokeWidth = 4
+
+function setup() {
+	// Cria o canvas
+	const cv = createCanvas(800, 600)
+	cv.position(600, 100)
+	cv.background(0)
+
+	// Inicia a conexão com o server via socket.io
+	socket = io.connect('http://localhost:3000')
+
+	// Função de callback
+	socket.on('mouse', data => {
+		stroke(data.color)
+		strokeWeight(data.strokeWidth)
+		line(data.x, data.y, data.px, data.py)
+	})
+
+	const color_picker = select('#pickcolor')
+	const color_btn = select('#color-btn')
+	const color_holder = select('#color-holder')
+
+	const stroke_width_picker = select('#stroke-width-picker')
+	const stroke_btn = select('#stroke-btn')
+
+	// Cria um listener para o botão que muda a cor do pincel
+	color_btn.mousePressed(() => {
+		// Verifica se o input é uma cor hexadecimal válida
+		if (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color_picker.value())) {
+			color = color_picker.value()
+			color_holder.style('background-color', color)
+		}
+		else {console.log('Enter a valid hex value')}
+	})
+
+	// Cria um listener para o botão que altera a espessura do pincel
+	stroke_btn.mousePressed(() => {
+		const width = parseInt(stroke_width_picker.value())
+		if (width > 0) strokeWidth = width
+	})
+}
+
+function mouseDragged() {
+	// Desenha
+	stroke(color)
+	strokeWeight(strokeWidth)
+	line(mouseX, mouseY, pmouseX, pmouseY)
+
+	// Envia as coordenadas do mouse
+	sendmouse(mouseX, mouseY, pmouseX, pmouseY)
+}
+
+// Envia as informações via socket.io
+function sendmouse(x, y, pX, pY) {
+	const data = {
+		x: x,
+		y: y,
+		px: pX,
+		py: pY,
+		color: color,
+		strokeWidth: strokeWidth,
+	}
+
+	socket.emit('mouse', data)
+}
---
+++ drawingapp/server.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+app.set('port', '3000')
+
+const server = http.createServer(app)
+server.on('listening', () => {
+    console.log('Listening on port 3000')
+})
+
+// Web socket
+const io = require('socket.io')(server)
+
+io.sockets.on('connection', (socket) => {
+	console.log('Client connected: ' + socket.id)
+	socket.on('mouse', (data) => socket.broadcast.emit('mouse', data))
+	socket.on('disconnect', () => console.log('Client has disconnected'))
+})
+
+server.listen('3000')
